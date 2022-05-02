b076aea80f54256c79c792d173997bb55b46e2aa
Ella <ellamartirosyan@gmail.com>
2022-05-02 13:15:21 +0300

    fix: script

--- run.sh
+++ run.sh
@@ -14,6 +14,6 @@ echo ovr:
 gdaladdo terrain.vrt -r bilinear -ro 2 4 8 16 32 64 128 256 512 1024 2048 4096 8192 16384 32768
 echo terrain:
 mkdir -p ./terrain
-tm ctb-tile -f Mesh -C -N -s 19 -e 0 -R -o ./terrain terrain.vrt
+tm ctb-tile -f Mesh -C -N -s 19 -e 0 -o ./terrain terrain.vrt
 echo layer.json:
 ctb-tile -f Mesh -C -N -s 19 -e 0 -l -o ./terrain terrain.vrt
