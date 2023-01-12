dd661faa93a1273bc1878449507dddc19e7fcc84
ellamartirosyan <ellamartirosyan@gmail.com>
2023-01-12 15:37:19 +0200

    fix: env vars

--- src/Components/FootprintVerification/FootprintVerification.tsx
+++ src/Components/FootprintVerification/FootprintVerification.tsx
@@ -1,138 +1,22 @@
 import React, { useState } from 'react';
 import { CesiumMap, CesiumSceneMode } from '@map-colonies/react-components';
 import { IBaseMaps } from '@map-colonies/react-components/dist/cesium-map/settings/settings';
+import appConfig from '../../Utils/Config';
 
 const FootprintVerification: React.FC = (): JSX.Element => {
 
-  const [center] = useState<[number, number]>([34.817, 31.911]);
+  const [center] = useState<[number, number]>(JSON.parse(appConfig.mapCenter));
 
-  const BASE_MAPS: IBaseMaps = {
-    "maps":[
-      {
-        "id":"1st",
-        "title":"1st Map",
-        "isCurrent":true,
-        "thumbnail":"https://nsw.digitaltwin.terria.io/build/3456d1802ab2ef330ae2732387726771.png",
-        "baseRasteLayers":[
-          {
-            "id":"GOOGLE_TERRAIN",
-            "type":"XYZ_LAYER",
-            "opacity":1,
-            "zIndex":0,
-            "options":{
-              "url":"https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-              "layers":"",
-              "credit":"GOOGLE"
-            }
-          },
-          {
-            "id":"INFRARED_RASTER",
-            "type":"WMS_LAYER",
-            "opacity":0.6,
-            "zIndex":1,
-            "options":{
-              "url":"https://mesonet.agron.iastate.edu/cgi-bin/wms/goes/conus_ir.cgi?",
-              "layers":"goes_conus_ir",
-              "credit":"Infrared data courtesy Iowa Environmental Mesonet",
-              "parameters":{
-                "transparent":"true",
-                "format":"image/png"
-              }
-            }
-          }
-        ],
-        "baseVectorLayers":[
-        ]
-      },
-      {
-        "id":"2nd",
-        "title":"2nd Map",
-        "thumbnail":"https://nsw.digitaltwin.terria.io/build/efa2f6c408eb790753a9b5fb2f3dc678.png",
-        "baseRasteLayers":[
-          {
-            "id":"RADAR_RASTER",
-            "type":"WMS_LAYER",
-            "opacity":0.6,
-            "zIndex":1,
-            "options":{
-              "url":"https://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi?",
-              "layers":"nexrad-n0r",
-              "credit":"Radar data courtesy Iowa Environmental Mesonet",
-              "parameters":{
-                "transparent":"true",
-                "format":"image/png"
-              }
-            }
-          },
-          {
-            "id":"GOOGLE_TERRAIN",
-            "type":"XYZ_LAYER",
-            "opacity":1,
-            "zIndex":0,
-            "options":{
-              "url":"https://mt1.google.com/vt/lyrs=s&x={x}&y={y}&z={z}",
-              "layers":"",
-              "credit":"GOOGLE"
-            }
-          },
-          {
-            "id":"VECTOR_TILES_GPS",
-            "type":"XYZ_LAYER",
-            "opacity":1,
-            "zIndex":2,
-            "options":{
-              "url":"https://gps.tile.openstreetmap.org/lines/{z}/{x}/{y}.png",
-              "layers":"",
-              "credit":"openstreetmap"
-            }
-          }
-        ],
-        "baseVectorLayers":[
-        ]
-      },
-      {
-        "id":"3rd",
-        "title":"3rd Map",
-        "thumbnail":"https://nsw.digitaltwin.terria.io/build/d8b97d3e38a0d43e5a06dea9aae17a3e.png",
-        "baseRasteLayers":[
-          {
-            "id":"VECTOR_TILES",
-            "type":"XYZ_LAYER",
-            "opacity":1,
-            "zIndex":0,
-            "options":{
-              "url":"https://{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png?apikey=6170aad10dfd42a38d4d8c709a536f38",
-              "layers":"",
-              "credit":"thunderforest"
-            }
-          },
-          {
-            "id":"VECTOR_TILES_GPS_1",
-            "type":"XYZ_LAYER",
-            "opacity":1,
-            "zIndex":1,
-            "options":{
-              "url":"https://gps.tile.openstreetmap.org/lines/{z}/{x}/{y}.png",
-              "layers":"",
-              "credit":"openstreetmap"
-            }
-          }
-        ],
-        "baseVectorLayers":[
-        ]
-      }
-    ]
-  };
+  const baseMaps: IBaseMaps = JSON.parse(appConfig.baseMaps);
 
   return (
     <CesiumMap
-      style={{height: '95%'}}
+      style={{height: '100%'}}
       center={center}
-      zoom={5}
+      zoom={+appConfig.mapZoom}
       sceneModes={[CesiumSceneMode.SCENE2D]}
-      baseMaps={BASE_MAPS}
-    >
-    </CesiumMap>
+      baseMaps={baseMaps}
+    />
   );
 
 };
