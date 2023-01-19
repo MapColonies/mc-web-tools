86a3c2afaa0c67ca6c33c29be3719cb64e245889
Ella <ellamartirosyan@gmail.com>
2023-01-19 05:40:34 +0200

    fix: fix

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ ENTERYPOINT=0
 # Taken from: https://pretzelhands.com/posts/command-line-flags
 for arg in "$@"
 do
-    case $arg in
-        --entrypoint)
-        ENTERYPOINT=1
-        shift # Remove
-        ;;
-    esac
+  case $arg in
+    --entrypoint)
+    ENTERYPOINT=1
+    shift # Remove
+    ;;
+  esac
 done
 
 if [ $ENTERYPOINT -eq 1 ]; then
