3967b3d184c206a431885457b2fc53e9c7d6f5b8
ellamartirosyan <ellamartirosyan@gmail.com>
2023-02-08 20:49:13 +0200

    fix: terrain should ignore error in case of a missing tile

--- src/Components/Terrain/Terrain.tsx
+++ src/Components/Terrain/Terrain.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { CesiumTerrainProvider, EllipsoidTerrainProvider } from 'cesium';
+import { get } from 'lodash';
 import { useCesiumMap } from '@map-colonies/react-components';
 import appConfig from '../../Utils/Config';
 
@@ -8,21 +9,32 @@ const Terrain: React.FC = () => {
   const mapViewer = useCesiumMap();
 
   useEffect(() => {
-    mapViewer.terrainProvider = new CesiumTerrainProvider({
-      url: appConfig.terrainProviderUrl,
-    });
+    const isTerrainTileError = (e: Record<string, unknown>): boolean => {
+      return get(e, 'level') as number > 0;
+    };
 
-    const terrainErrorEvent = mapViewer.terrainProvider.errorEvent;
-
-    function handleTerrainError(e: unknown): void {
-      if (appConfig.terrainProviderUrl) {
-        alert(`\nTerrain Provider Access Error\n\n${appConfig.terrainProviderUrl}`);
+    const handleTerrainError = (e: Record<string, unknown>): void => {
+      if (!isTerrainTileError(e)) {
+        console.error('Terrain provider error: Falling back to default terrain.', e);
+        alert(`\nTerrain Provider Access Error:\n\n${appConfig.terrainProviderUrl}`);
+        // Remove error event listener after failing once
+        mapViewer.terrainProvider.errorEvent.removeEventListener(handleTerrainError);
+        // Set default empty terrain provider
+        mapViewer.terrainProvider = new EllipsoidTerrainProvider({});
+      } else {
+        console.error('Terrain provider error: Tile problem.', e);
       }
-      mapViewer.terrainProvider = new EllipsoidTerrainProvider({});
-      terrainErrorEvent.removeEventListener(handleTerrainError);
-    }
+    };
 
-    terrainErrorEvent.addEventListener(handleTerrainError);
+    if (appConfig.terrainProviderUrl) {
+      mapViewer.terrainProvider = new CesiumTerrainProvider({
+        url: appConfig.terrainProviderUrl
+      });
+      mapViewer.terrainProvider.errorEvent.addEventListener(handleTerrainError);
+    } else {
+      alert(`\nTerrain Provider Access Error:\n\nEmpty Terrain URL`);
+      mapViewer.terrainProvider = new EllipsoidTerrainProvider({});
+    }
   }, []);
 
   return null;
