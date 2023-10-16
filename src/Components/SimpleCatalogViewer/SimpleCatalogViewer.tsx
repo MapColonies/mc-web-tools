06a76a4539a5138e2eca4bf80b762ad1ce64d495
assafchamoy <assafchamoy97@gmail.com>
2023-10-16 11:38:07 +0300

    feat: show model extent

--- src/Components/SimpleCatalogViewer/SimpleCatalogViewer.tsx
+++ src/Components/SimpleCatalogViewer/SimpleCatalogViewer.tsx
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import { AxiosResponse } from "axios";
-import { Cesium3DTileset, CesiumMap, CesiumSceneMode } from "@map-colonies/react-components";
+import {
+    Cesium3DTileset,
+    CesiumColor,
+    CesiumConstantProperty,
+    CesiumGeojsonLayer,
+    CesiumMap,
+    CesiumSceneMode
+} from "@map-colonies/react-components";
 import { requestHandlerWithToken } from "./utils/requestHandler";
 import { getRecordsQueryByID, parseQueryResults } from "./utils/cswQueryBuilder";
 import appConfig from "../../Utils/Config";
@@ -9,6 +16,7 @@ import { useQueryParams } from "../../Hooks/useQueryParams";
 
 import "./SimpleCatalogViewer.css";
 import { validateIDsQuery, validatePositionQuery } from "./utils/validateQueryParams";
+import { getFootprintsCollection } from "./utils";
 
 const MAXIMUM_SCREEN_SPACE_ERROR = 5;
 const CULL_REQUESTS_WHILE_MOVING_MULTIPLIER = 120;
@@ -19,6 +27,7 @@ interface IClientFlyToPosition {
 
 const SimpleCatalogViewer: React.FC = (): JSX.Element => {
     const [models, setModels] = useState<Record<string, unknown>[]>([]);
+    const [footprints, setFootprints] = useState<Record<string, unknown>[]>([]);
     // const [isLoading, setIsLoading] = useState(true);
     const queryParams = useQueryParams();
 
@@ -26,10 +35,14 @@ const SimpleCatalogViewer: React.FC = (): JSX.Element => {
     let modelIds: string[] = [];
     let idQueried: string | null = queryParams.get("model_ids");
     if (idQueried == null) {
-        console.error(`Error: model_ids does not exists in the query params!\nA good example: "http://url?model_ids=ID1,ID2"`);
+        console.error(
+            `Error: model_ids does not exists in the query params!\nA good example: "http://url?model_ids=ID1,ID2"`
+        );
     } else {
         if (!validateIDsQuery(idQueried)) {
-            console.error(`Error: model_ids does not fit the specification!\nA good example: "http://url?model_ids=ID1,ID2"`);
+            console.error(
+                `Error: model_ids does not fit the specification!\nA good example: "http://url?model_ids=ID1,ID2"`
+            );
         } else {
             modelIds = idQueried.split(",");
 
@@ -41,8 +54,9 @@ const SimpleCatalogViewer: React.FC = (): JSX.Element => {
     const positionQueried: string | null = queryParams.get("position");
     if (positionQueried != null) {
         if (!validatePositionQuery(positionQueried)) {
-            console.error(`Error: position does not fit the specification!\nA good example: "http://url?position=lon,lat,height"\nP.S\nThe position param is optional`);
-
+            console.error(
+                `Error: position does not fit the specification!\nA good example: "http://url?position=lon,lat,height"\nP.S\nThe position param is optional`
+            );
         } else {
             const parsedPosition = positionQueried.split(",");
             clientPosition = {
@@ -53,35 +67,46 @@ const SimpleCatalogViewer: React.FC = (): JSX.Element => {
     }
     const userToken = queryParams.get("token");
     if (userToken === null) {
-        console.error(`Error: No token was provided. The token should be as a query param with the name "token".\nA good example: "http://url?token=TOKEN"`);
-
+        console.error(
+            `Error: No token was provided. The token should be as a query param with the name "token".\nA good example: "http://url?token=TOKEN"`
+        );
     }
 
+    console.log(queryParams.get("show_extent"))
+
+    const shouldShowExtent = JSON.parse(queryParams.get("show_extent") || "false");
+
     useEffect(() => {
-        if(modelIds.length > 2) {
-        console.warn("You provided more than 2 models. This is not recommended");
+        if (modelIds.length > 2) {
+            console.warn("You provided more than 2 models. This is not recommended");
         }
 
-        if(userToken) {
-          const cswRequestHandler = async (
-              url: string,
-              method: string,
-              params: Record<string, unknown>
-          ): Promise<AxiosResponse> =>
-              requestHandlerWithToken(appConfig.simpleCatalogViewerTool.csw3dUrl, method, params, userToken);
-  
-          cswRequestHandler(appConfig.simpleCatalogViewerTool.csw3dUrl, "POST", {
-              data: getRecordsQueryByID(modelIds, "http://schema.mapcolonies.com/3d")
-          })
-              .then((res) => {
-                  let modelsResponse = parseQueryResults(res.data, "mc:MC3DRecord");
-                  if (modelsResponse !== null) {
-                      setModels(modelsResponse);
-                  }
-              })
-              .catch((e) => {
-                  console.error(e);
-              })
+        if (userToken) {
+            const cswRequestHandler = async (
+                url: string,
+                method: string,
+                params: Record<string, unknown>
+            ): Promise<AxiosResponse> =>
+                requestHandlerWithToken(
+                    appConfig.simpleCatalogViewerTool.csw3dUrl,
+                    method,
+                    params,
+                    userToken
+                );
+
+            cswRequestHandler(appConfig.simpleCatalogViewerTool.csw3dUrl, "POST", {
+                data: getRecordsQueryByID(modelIds, "http://schema.mapcolonies.com/3d")
+            })
+                .then((res) => {
+                    let modelsResponse = parseQueryResults(res.data, "mc:MC3DRecord");
+                    if (modelsResponse !== null) {
+                        setModels(modelsResponse);
+                        setFootprints(modelsResponse.map(model => JSON.parse(model["mc:footprint"] as string)));
+                    }
+                })
+                .catch((e) => {
+                    console.error(e);
+                });
             //   .finally(() => {
             //       setIsLoading(false);
             //   });
@@ -125,6 +150,23 @@ const SimpleCatalogViewer: React.FC = (): JSX.Element => {
                         />
                     );
                 })}
+                
+                {shouldShowExtent && 
+                    <CesiumGeojsonLayer
+                        clampToGround={true}
+                        data={getFootprintsCollection(footprints, true)}
+                        onLoad={(geojsonDataSource) => {
+                            geojsonDataSource.entities.values.forEach((item) => {
+                                if (item.polyline) {
+                                    const color = CesiumColor.CYAN;
+                                    (item.polyline.width as CesiumConstantProperty).setValue(5);
+                                    // @ts-ignore
+                                    item.polyline.material = color;
+                                }
+                            })
+                        }}
+                    />
+                }
                 <Terrain />
             </CesiumMap>
         </>
