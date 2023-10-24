f640bc94ec3d54c8f3a14403529f3c0865a2bce2
alebinson <newdarkvirus@gmail.com>
2023-10-24 13:11:32 +0300

    fix: chrome 70 globalthis polyfill

---
+++ public/assets/scripts/polyfill.globalThis.js
@@ -0,0 +1,13 @@
+// The polyfill starts here.
+(function() {
+    if (typeof globalThis === 'object') return;
+    Object.defineProperty(Object.prototype, '__magic__', {
+        get: function() {
+            return this;
+        },
+        configurable: true
+    });
+    __magic__.globalThis = __magic__;
+    delete Object.prototype.__magic__;
+}());
+// The polyfill ends here.
